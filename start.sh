6ca7cfdbb6f0c781ba6aae8132a136d524344996
Greymattersbot <110471652+Greymattersbot@users.noreply.github.com>
2022-10-28 19:27:34 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/Greymattersbot/abke-tuje-dund-lete-hai /MdiskBot
+cd /MdiskBot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
